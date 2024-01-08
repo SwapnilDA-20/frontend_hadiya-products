3266094332dfaa42dd58763b084439e27aae41d9
SwapnilDA-20 <69837664+SwapnilDA-20@users.noreply.github.com>
2024-01-08 15:23:23 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiGatewayURL: 'http://54.86.214.197:3000'
+  apiGatewayURL: 'https://ecs-alb-387026215.us-east-1.elb.amazonaws.com:3000'
 };
 
 /*
